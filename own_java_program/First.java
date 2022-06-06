6713679382fb639aa0bc3185613907c424985f2b
Aaron Phillips <101356609+aaronbasha@users.noreply.github.com>
2022-06-06 11:54:01 -0700

    Add files via upload

---
+++ own_java_program/First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) {
+		System.out.println("My Name is Aaron");
+        System.out.println("I am Old");
+        System.out.println("Home Town is Palo Alto");
+	} 
+}
+
